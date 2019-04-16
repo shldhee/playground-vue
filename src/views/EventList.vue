8ff8afdd5c9264f74b34562d4695415fccdaba61
duckhee <shldhee@yahoo.co.kr>
2019-04-16 16:29:50 +0900

    Completed Single Components

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Event Listing</h1>
+    <EventCard />
     <p>
       <router-link :to="{ name: 'event-show', params: { id: '1' } }"
         >First Event</router-link
@@ -10,7 +11,13 @@
 </template>
 
 <script>
-export default {}
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
