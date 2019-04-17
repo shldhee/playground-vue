ce70ecd7a9df23d9b7399a0d100681662fc52511
duckhee <shldhee@yahoo.co.kr>
2019-04-17 17:58:00 +0900

    Studying Fleshing out BaseIcon.vue

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Icon
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,33 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+// Vue.component('BaseIcon', BaseIcon)
+
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+console.log(requireComponent.keys())
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+  console.log(componentConfig)
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  )
+
+  console.log(componentName)
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+  console.log(componentConfig.default)
+  console.log(componentConfig)
+})
 
 Vue.config.productionTip = false
 
