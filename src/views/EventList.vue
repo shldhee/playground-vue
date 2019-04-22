9aa844781b3b569a9a20365d9578792b532ba88e
shldhee <shldhee@yahoo.co.kr>
2019-04-22 21:41:44 +0900

    Completed Real World Vue.js course

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,22 +1,34 @@
 <template>
   <div>
     <h1>Event Listing</h1>
-
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
     <p>
-      <router-link :to="{ name: 'event-show', params: { id: '1' } }"
-        >First Event</router-link
-      >
+      <router-link :to="{ name: 'event-show', params: { id: '1' } }">First Event</router-link>
     </p>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventServices.js'
 
 export default {
   components: {
     EventCard
+  },
+  data: function() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was error', error.message)
+      })
   }
 }
 </script>
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,13 +1,74 @@
 <template>
   <div>
-    <h1>Showing event #{{ id }}</h1>
+    <div class="event-header">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h1 class="title">{{ event.title }}</h1>
+      <h5>Organized by {{ event.organizer }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+    <BaseIcon name="map">
+      <h2>Location</h2>
+    </BaseIcon>
+    <address>{{ event.location }}</address>
+    <h2>Event details</h2>
+    <p>{{ event.description }}</p>
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
+    </h2>
+    <ul class="list-group">
+      <li
+        v-for="(attendee, index) in event.attendees"
+        :key="index"
+        class="list-item"
+      >
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import EventService from '@/services/EventServices.js'
 export default {
-  props: ['id']
+  props: ['id'],
+  data: function() {
+    return {
+      event: []
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+        console.log(this.event)
+      })
+      .catch(error => {
+        console.log('There was error : ', error.message)
+      })
+  }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.event-header > .title {
+  margin: 0;
+}
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+</style>
